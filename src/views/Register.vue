87b693f5f3795b315f0493edb31620a4fceaf78e
fariqM <maulana.fariq30@gmail.com>
2023-05-10 12:11:28 +0700

    updaet

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,78 @@
 <template>
-  <div>register page</div>
+	<v-container>
+		<v-row>
+			<v-col cols="12" sm="2">
+				<v-sheet class="bg-deep-grey pa-3" rounded>
+					<v-card class="mx-auto px-6 py-8" max-width="344">
+						<v-form v-model="form" @submit.prevent="onSubmit">
+							<v-text-field
+								v-model="email"
+								:readonly="loading"
+								:rules="[required]"
+								class="mb-2"
+								clearable
+								label="Username"
+							></v-text-field>
+
+							<v-text-field
+								v-model="password"
+								:readonly="loading"
+								:rules="[required]"
+								clearable
+								label="Name"
+								placeholder="Enter your password"
+							></v-text-field>
+
+							<v-text-field
+								v-model="name"
+								:readonly="loading"
+								:rules="[required]"
+								clearable
+								label="Password"
+								placeholder="Enter your password"
+							></v-text-field>
+							<br />
+
+							<v-btn
+								:disabled="!form"
+								:loading="loading"
+								block
+								color="success"
+								size="large"
+								type="submit"
+								variant="elevated"
+							>
+								Sign In
+							</v-btn>
+						</v-form>
+					</v-card>
+				</v-sheet>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 export default {
+	data: () => ({
+		form: false,
+		email: null,
+		password: null,
+		name: null,
+		loading: false,
+	}),
 
-}
+	methods: {
+		onSubmit() {
+			if (!this.form) return;
+			this.loading = true;
+			setTimeout(() => (this.loading = false), 2000);
+		},
+		required(v) {
+			return !!v || "Field is required";
+		},
+	},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
