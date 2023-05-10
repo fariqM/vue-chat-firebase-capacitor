9fdd8ada95f2d08d0b667a5859bcce97ff987bf5
fariqM <maulana.fariq30@gmail.com>
2023-05-10 12:09:32 +0700

    updaet

---
+++ src/layouts/Guest.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-app id="inspire">
+        <v-app-bar class="px-3" color="white" flat density="compact">
+
+            <v-spacer></v-spacer>
+
+            <v-tabs centered color="grey-darken-2">
+                <v-tab v-for="link in links" :key="link" :to="{ name: link.url }">
+                    {{ link.title }}
+                </v-tab>
+            </v-tabs>
+            <v-spacer></v-spacer>
+        </v-app-bar>
+
+        <v-main class="bg-grey-lighten-3">
+            <router-view />
+        </v-main>
+    </v-app>
+</template>
+  
+<script>
+export default {
+    data: () => ({
+        links: [
+            {
+                title: 'Login',
+                url: 'login'
+            },
+            {
+                title: 'Register',
+                url: 'register'
+            }
+        ],
+    }),
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    component: () => import('@/layouts/Guest.vue'),
     children: [
       {
         path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        name: 'login',
+        component: () => import('@/views/Login.vue'),
+      },
+      {
+        path: 'register',
+        name: 'register',
+        component: () => import('@/views/Register.vue'),
       },
     ],
   },
